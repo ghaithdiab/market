105b29b0892227c05240390d7892c08540b87d6f
ghaith <ghaith.diab@hotmail.com>
2023-03-26 00:13:46 +0100

    add error page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Admin from './pages/Admin';
 import Home from './pages/Home';
 import LoginUser from './pages/LoginUser';
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<LoginUser/>}></Route>
         <Route path='/admin' element={<Admin/>}></Route>
+        <Route path='*' element={<ErrorPage/>}></Route>
       </Routes>
     </div>
   );
